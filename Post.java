4fc5d9ccfd415300bc9a9d43b3d4981f92101903
ttroy26 <ttroy@hotmail.com>
2019-10-19 00:46:35 +0300

    post java degistirildi

---
+++ Post.java
@@ -0,0 +1,22 @@
+package com.example.drana_000.bluetoothyoklamasistemi;
+
+/**
+ * Created by Drana_000 on 17.5.2017.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Post {
+    public static String gonder(String site , String json)
+    {
+		int b=5+4;
+       return null;
+    }
+}
